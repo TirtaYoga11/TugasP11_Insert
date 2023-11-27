2ca8a3da3c76763aa9ad2a468fa143ac59d54f66
TirtaYoga11 <97031913+TirtaYoga11@users.noreply.github.com>
2023-11-27 21:12:54 +0800

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020010_tirtayoga";
+
+    $con    = mysqli_connect($server,$user,$password,$database);
+
+    ?>
